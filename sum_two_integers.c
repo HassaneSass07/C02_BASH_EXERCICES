fc62c4f0c8556779696cff94c95ac0400f6930b4
idriss <idrissaamadouousmane@gmail.com>
2025-05-16 22:24:43 +0100

    dhjf

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,17 +1,27 @@
 #include <stdio.h>  // Inclusion de la bibliothèque standard pour les entrées/sorties
 
 int main() {
+//    int a, b, somme;
+//
+//    // Demande à l'utilisateur de saisir deux entiers
+//    printf("Entrez deux entiers : ");
+//    scanf("%d" "%d", &a, &b);  // Lecture des deux entiers
+//
+//    // Calcul de la somme
+//    somme = a + b;
+//
+//    // Affichage du résultat
+//    printf("La somme est : %d\n", somme);
+//
+//    return 0;  // Fin normale du programme
+//}
+
     int a, b, somme;
 
-    // Demande à l'utilisateur de saisir deux entiers
     printf("Entrez deux entiers : ");
-    scanf("%d %d", &a, &b);  // Lecture des deux entiers
+    scanf("%d" "%d", &a, &b);
 
-    // Calcul de la somme
     somme = a + b;
-
-    // Affichage du résultat
     printf("La somme est : %d\n", somme);
-
-    return 0;  // Fin normale du programme
-}
+    return 0;
+    }
