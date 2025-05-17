5360f63aa5aa840fae362f73b337ff7dfb9b311a
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 03:08:41 +0100

    encore

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -5,13 +5,16 @@ int main(void)
     int a, b;
 
     // Lire deux entiers depuis l'entrée standard
-    scanf("%d %d", &a, &b);
-        
+    if (scanf("%d %d", &a, &b) != 2) {
+        // Vérification de la saisie
+        printf("Erreur de saisie.\n");
+        return 1;
+    }
+
     // Échange des valeurs sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
-    b = a + b;
 
     // Affichage du résultat comme demandé
     printf("Après échange: a = %d, b = %d\n", a, b);
