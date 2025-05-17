52149ce920ac1659ef53839fd0283a08c2df947e
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 04:35:53 +0100

    sdhgt

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -4,10 +4,10 @@ int main(void) {
     int a, b;
 
     // Demande à l'utilisateur de saisir deux entiers
-    scanf("%d" "%d", &a, &b);
+    scanf("%d %d", &a, &b);
 
     // Affiche la somme des deux entiers
     printf("La somme est : %d\n", a + b);
 
-    return 1;
+    return 0;
 }
