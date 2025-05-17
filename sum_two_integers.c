0270a338f7bc5ebb9565005e9788a99d94afeb27
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 04:44:04 +0100

    erftgd

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -3,12 +3,10 @@
 int main(void) {
     int a, b;
 
-    // Lecture des deux entiers sans message
-    if (scanf("%d %d", &a, &b) != 2) {
-        return 1; // erreur de saisie
-    }
+    // Demande à l'utilisateur de saisir deux entiers
+    scanf("%d %d", &a, &b);
 
-    // Affichage exact demandé
+    // Affiche la somme des deux entiers
     printf("La somme est : %d\n", a + b);
 
     return 0;
--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int a, b;
 
-    // Lecture des deux entiers sans message
-    if (scanf("%d %d", &a, &b) != 2) {
-        return 1; // erreur de saisie
-    }
+    scanf("%d %d", &a, &b);
 
-    // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
 
-    // Affichage strictement conforme
     printf("Après échange: a = %d, b = %d\n", a, b);
 
     return 0;
