4246c962ba3cfd2211aa3774d2491e20fbdbd569
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 04:06:49 +0100

    ooppd

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,18 +2,19 @@
 
 int main(void)
 {
-    int a, b, temp;
+    int a, b;
 
     // Lire deux entiers depuis l'entrée standard
     if (scanf("%d %d", &a, &b) != 2) {
+        // Vérification de la saisie
         printf("Erreur de saisie.\n");
         return 1;
     }
 
-    // Échange avec variable temporaire
-    temp = a;
-    a = b;
-    b = temp;
+    // Échange des valeurs sans variable temporaire
+    a = a + b;
+    b = a - b;
+    a = a - b;
 
     // Affichage du résultat comme demandé
     printf("Après échange: a = %d, b = %d\n", a, b);
