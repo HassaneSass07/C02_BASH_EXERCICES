6e6b56194b241b2fbb1ccb7a373512e5f451a761
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 04:29:17 +0100

    sdfxcv

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -4,15 +4,12 @@ int main(void)
 {
     int a, b;
 
-    // Lecture des deux entiers sans message
     scanf("%d %d", &a, &b);
 
-    // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
 
-    // Affichage conforme à l’énoncé
     printf("Après échange: a = %d, b = %d\n", a, b);
 
     return 0;
