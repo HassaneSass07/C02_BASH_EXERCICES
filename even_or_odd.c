d9a04b4410f4fc9d7ba047098dd86c5035207755
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 03:52:06 +0100

    zzzzzzzzzzee

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int nombre;
 
-    // Lecture d'un entier
+    // Lire un entier depuis l'entrée standard
     scanf("%d", &nombre);
 
-    // Vérifie si le nombre est pair ou impair et affiche le résultat
+    // Vérifier si le nombre est pair ou impair
     if (nombre % 2 == 0)
+    {
         printf("%d est un nombre pair.\n", nombre);
+    }
     else
+    {
         printf("%d est un nombre impair.\n", nombre);
+    }
 
     return 0;
 }
