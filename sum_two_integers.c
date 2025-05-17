168671d6224cfa179be1665ca6c07e2e20a59d51
idriss <idrissaamadouousmane@gmail.com>
2025-05-17 03:13:57 +0100

    sdxcnvb

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>  // Inclusion de la bibliothèque standard pour les entrées/sorties
 
-int main(void) {
+int main() {
     int a, b, somme;
 
     // Demande à l'utilisateur de saisir deux entiers
-    printf("Entrez deux entiers:");
+    printf("Entrez deux entiers : ");
     scanf("%d""%d", &a, &b);  // Lecture des deux entiers
 
     // Calcul de la somme
-    somme= a + b;
+    somme = a + b;
 
     // Affichage du résultat
-    printf("La somme est:%d\n",somme);
+    printf("La somme est : %d\n", somme);
 
     return 0;  // Fin normale du programme
 }
--- swap_no_temp.c
+++ swap_no_temp.c
@@ -4,19 +4,15 @@ int main(void)
 {
     int a, b;
 
-    // Lire deux entiers depuis l'entrée standard
-    if (scanf("%d %d", &a, &b) != 2) {
-        // Vérification de la saisie
-        printf("Erreur de saisie.\n");
-        return 1;
-    }
+    // Lecture de deux entiers sans message préalable
+    scanf("%d %d", &a, &b);
 
-    // Échange des valeurs sans variable temporaire
+    // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
 
-    // Affichage du résultat comme demandé
+    // Affichage exactement conforme à l'exemple
     printf("Après échange: a = %d, b = %d\n", a, b);
 
     return 0;
